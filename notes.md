1df07eb92de46425090d2b9f52b387d54b5aaabe
vinitian <vinitajwp@gmail.com>
2024-04-11 11:46:23 +0700

    feat: add dropdown and doughnutChart, add notes.md

---
+++ notes.md
@@ -0,0 +1,7 @@
+what we used in this project
+1. [chartjs.org](chartjs.org)
+2. data from [here](https://data.thailand.opendevelopmentmekong.net/th/dataset/thailand-population-2019/resource/108125ab-7323-4b29-bcbe-9fc8bb24acbf)
+3. the actual API [here](https://data.thailand.opendevelopmentmekong.net/th/api/3/action/datastore_search?resource_id=108125ab-7323-4b29-bcbe-9fc8bb24acbf)
+3. [how to make dropdown](https://www.w3schools.com/tags/tag_select.asp)
+4. [var vs. let](https://stackoverflow.com/questions/762011/what-is-the-difference-between-let-and-var)
+5. [.map(), .filter()](https://stackoverflow.com/questions/57699996/how-to-map-with-a-conditional-in-javascript)
--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-async function showExchangeRateGraph(){
-    const response = await fetch("https://www.floatrates.com/daily/usd.json");
-    const fromCurrency = document.getElementById("fromCurrency");
-    const toCurrency = document.getElementById("toCurrency");
+// async function createOptions(){
+//     const response = await fetch("https://data.thailand.opendevelopmentmekong.net/th/api/3/action/datastore_search?resource_id=108125ab-7323-4b29-bcbe-9fc8bb24acbf")
+//     const wholeData = await response.json();
+//     const provinces = wholeData.result.records.map(e => e.province)
+//     const dropdown = document.getElementById("province");
+//     for (let i = 0; i < provinces.length; i++){
+//         var option = document.createElement("option");
+//         option.textContent = provinces[i];
+//         option.value = i;
+//         dropdown.appendChild(option);
+//         //console.log(provinces[i]);
+//     }
+// }
+
+async function createOptions(){
+    const provinces = ['Amnat Charoen', 'Ang Thong', 'Bangkok', 'Bueng Kan', 'Buri Ram', 'Chachoengsao', 'Chai Nat', 'Chaiyaphum', 'Chanthaburi', 'Chiang Mai', 'Chiang Rai', 'Chon Buri', 'Chumphon', 'Kalasin', 'Kamphaeng Phet', 'Kanchanaburi', 'Khon Kaen', 'Krabi', 'Lampang', 'Lamphun', 'Loei', 'Lop Buri', 'Mae Hong Son', 'Maha Sarakham', 'Mukdahan', 'Nakhon Nayok', 'Nakhon Pathom', 'Nakhon Phanom', 'Nakhon Ratchasima', 'Nakhon Sawan', 'Nakhon Si Thammarat', 'Nan', 'Narathiwat', 'Nong Bua Lam Phu', 'Nong Khai', 'Nonthaburi', 'Pathum Thani', 'Pattani', 'Phangnga', 'Phatthalung', 'Phayao', 'Phetchabun', 'Phetchaburi', 'Phichit', 'Phitsanulok', 'Phra Nakhon Si Ayutthaya', 'Phrae', 'Phuket', 'Prachin Buri', 'Prachuap Khiri Khan', 'Ranong', 'Ratchaburi', 'Rayong', 'Roi Et', 'Sa Kaeo', 'Sakon Nakhon', 'Samut Prakan', 'Samut Sakhon', 'Samut Songkhram', 'Saraburi', 'Satun', 'Si Sa Ket', 'Sing Buri', 'Songkhla', 'Sukhothai', 'Suphan Buri', 'Surat Thani', 'Surin', 'Tak', 'Trang', 'Trat', 'Ubon Ratchathani', 'Udon Thani', 'Uthai Thani', 'Uttaradit', 'Yala', 'Yasothon'];
+    const dropdown = document.getElementById("province");
+    for (let i = 0; i < provinces.length; i++){
+        var option = document.createElement("option");
+        option.textContent = provinces[i];
+        option.value = i;
+        dropdown.appendChild(option);
+        //console.log(provinces[i]);
+    }
 }
 
-async function importNames(){
-    console.log(currencies);
-}
+//to-do: make the chart updatable
+async function showDoughnutChart(provinces){
+    const response = await fetch("https://data.thailand.opendevelopmentmekong.net/th/api/3/action/datastore_search?resource_id=108125ab-7323-4b29-bcbe-9fc8bb24acbf")
+    const wholeData = await response.json();
+    const provinceNames = wholeData.result.records
+        .filter(e => provinces.includes(e._id.toString()))
+        .map(x => x.province);
+    const populationData = wholeData.result.records
+        .filter(e => provinces.includes(e._id.toString()))
+        .map(x => x.total);
 
-importNames();
+    let theRestPop = 66558935;
+    populationData.forEach(e => theRestPop -= e);
+    populationData.push(theRestPop);
+    provinceNames.push("The rest of population");
+
+    // console.log(provinceNames);
+    // console.log(populationData);
+
+    new Chart(doughnutChart, {
+        type:'doughnut',
+        data: {
+          labels: provinceNames,
+          datasets: [{
+            label: 'Population',
+            data: populationData,
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 50
+          }]
+        }
+      });
+}
